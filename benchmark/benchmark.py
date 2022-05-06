12eabec497412432577c2ae47ede9f98814822cf
Jaroslav P <jarko.paska@gmail.com>
2022-05-06 15:21:28 +0100

    update readme, refactor benchmark

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,22 +1,22 @@
+import os
 import random
+import subprocess
+import sys
+import time
+
 random.seed(0)
 n = 1
 
-print('Writing test files')
 for i in range(n):
-    with open(f'{i}.in', 'w') as f:
-        f.write(''.join([str(random.randint(0, 1)) for _ in range(100_000_000)]))
+    filepath = f'files/{i}.in'
+    if not os.path.exists(filepath):
+        print(f'Writing {filepath}')
+        with open(filepath, 'w') as f:
+            f.write(''.join([str(random.randint(0, 1)) for _ in range(100_000_000)]))
 
-print('Running minigrep')
-import subprocess, sys, time
-t0 = time.time()
 out = open('out', 'w')
+print('Running minigrep')
+t0 = time.time()
 subprocess.run([sys.argv[1], '.', '111'], stdout=out)
 out.close()
 print(f'{time.time() - t0} seconds elapsed')
-
-print('Removing files')
-import os
-for i in range(n):
-    os.remove(f'{i}.in')
-os.remove('out')
